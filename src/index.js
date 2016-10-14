657940e43d2eaca38fa1ffd30c79a8341635d115
KYUNG MO KWEON <kkweon@gmail.com>
2016-10-13 23:42:48 -0700

    inputbox

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar'
+const API_KEY = "AIzaSyB1of5SR7s2CT-zBtRdHCSVnoGPCP4puVI"
 /*
 Create a new component.
 This component should produce some HTML.
-
 */
-import React from 'react';
-import ReactDOM from 'react-dom';
 const App = () => {
-    return <div>Hi~</div>;
-}
-
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
+};
 // Take this component's generated HTML and put it on the page
 // on the page (in the DOM)
-
 ReactDOM.render(<App />, document.querySelector('.container'));
