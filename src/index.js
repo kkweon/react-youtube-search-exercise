594bfab3ff3c5cf9fa39168b739e127cb485ae06
KYUNG MO KWEON <kkweon@gmail.com>
2016-10-14 19:32:56 -0700

    youtube search app

--- src/index.js
+++ src/index.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar'
-const API_KEY = "AIzaSyB1of5SR7s2CT-zBtRdHCSVnoGPCP4puVI"
-/*
-Create a new component.
-This component should produce some HTML.
-*/
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
-};
-// Take this component's generated HTML and put it on the page
-// on the page (in the DOM)
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import _ from 'lodash';
+
+const API_KEY = "AIzaSyB1of5SR7s2CT-zBtRdHCSVnoGPCP4puVI";
+
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { 
+            videos: [],
+            selectedVideo: null,
+        };
+
+        this.videoSearch('tensorflow');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            // 아래는 아래와 같다.
+            // this.setState({ videos: videos});
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0],
+            }); 
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        );
+    };
+
+}
+
+
+
 ReactDOM.render(<App />, document.querySelector('.container'));
