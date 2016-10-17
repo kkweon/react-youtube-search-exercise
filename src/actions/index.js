348955a7014cc577270c3dd89e9f61b37b9d1e73
KYUNG MO KWEON <kkweon@gmail.com>
2016-10-17 01:49:56 -0700

    book detail view

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+// Action Creator
+
+export function selectBook(book) {
+    console.log("A book has been selected: ", book.title);
+    // selectBook is an action creator
+    // it needs to return an action, an object with a type property
+    return {
+        type: 'BOOK_SELECTED', //Always uppercase
+        payload: book,
+    };
+}
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title: "Javascript: The Good Parts"},
-        { title: "Harry Porter"},
-        { title: "Dark Tower"},
-        { title: "Eloquent Ruby"},
+        { title: "Javascript: The Good Parts", pages:100},
+        { title: "Harry Porter", pages: 500},
+        { title: "Dark Tower", pages: 85},
+        { title: "Eloquent Ruby", pages: 1},
     ];
 }
